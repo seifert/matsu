8342d1c38a58009486dbd345034987209402d997
Jan Seifert <jan.seifert@fotkyzcest.net>
2017-08-05 23:14:21 +0200

    add model admin for Category model

--- matsu/core/admin.py
+++ matsu/core/admin.py
@@ -1,3 +1,13 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from matsu.core.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'tree_path')
+    ordering = ('id',)
+    search_fields = ('title',)
+
