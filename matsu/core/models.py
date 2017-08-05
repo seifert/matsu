c3354489fa5467c6821c18a6557ef38d25e39af0
Jan Seifert <jan.seifert@fotkyzcest.net>
2017-08-05 23:10:42 +0200

    add Category model

--- matsu/core/models.py
+++ matsu/core/models.py
@@ -1,3 +1,21 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+
+class Category(models.Model):
+
+    title = models.CharField(_("Title"), max_length=50)
+    tree_path = models.SlugField(_("Tree path"), max_length=255)
+    content = models.TextField(_("Content"), blank=True)
+
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', args=[self.tree_path])
